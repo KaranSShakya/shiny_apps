e26e1441b1e22a1f48564e2a6e631fb1ea4fcad3
KaranSShakya <karan.fingo@gmail.com>
2020-07-11 14:19:25 -0700

    Import page attempt 1

--- code/2.test-import.R
+++ code/2.test-import.R
@@ -5,6 +5,31 @@ library(readr)
 library(readxl)
 
 # ui ----
-shinyUI(fluidPage(
-  headerPanel()
+ui <- shinyUI(fluidPage(
+  headerPanel(title = "Upload File"),
+  sidebarLayout(
+    sidebarPanel(
+      fileInput("file", "Upload the File"),
+      h5("File has to be CSV and pre-organized"),
+    ),
+    mainPanel(
+      tableOutput("input_file")
+    )
+  )
 ))
+
+# server ----
+server <- shinyServer(function(input, output){
+  
+  output$input_file <- renderTable({
+    file_to_read = input$file
+    if (is.null(file_to_read)){
+      return()
+    }
+    use.table <- read.table(file_to_read$datapath, sep=",", header=T)
+    main.data <- as.data.frame(use.table) %>% 
+      head(10)
+  })
+})
+
+shinyApp(ui, server) 
